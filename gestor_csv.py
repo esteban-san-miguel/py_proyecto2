f5d32cef3d3d84905451b5787b6688b01aea2250
Esteban <estebansanmiguel78@gmail.com>
2025-08-07 00:11:14 -0300

    v1

--- gestor_csv.py
+++ gestor_csv.py
@@ -1,8 +1,8 @@
 import csv
 import os
 
-class Gestor_CSV:
-    def __init__(self, archivo):
+class Gestor_CSV: # clase
+    def __init__(self, archivo): # constructor (propiedades y estado inicial del objeto)
         self.archivo = archivo
         self.headers = [
             "fecha", "nombre", "apellido", "celular", "email", 
@@ -12,9 +12,15 @@ class Gestor_CSV:
         ]
         self.inicializar_archivo()
 
-    def inicializar_archivo(self): # método de clase
+    def inicializar_archivo(self): # método de clase (acciones que realizará el objeto)
         if not os.path.exists(self.archivo): # verifica si existe el archivo
             with open(self.archivo, "w", newline="") as f: # abre el archivo en modo escritura
                 writer = csv.writer(f) # crea el archivo y escribe las columnas
                 writer.writerow(self.headers) # escribe los encabezados en la fila 1 (0)
                 
+    def guardar(self, fila): # método de clase c/2 args (fila es un diccionario contenedor del archivo csv)
+        with open(self.archivo, "a", newline="") as f: # abre, agrega y cierra el archivo csv
+            writer = csv.DictWriter(f, fieldnames=self.headers) # escribe datos en el archivo desde diccionarios
+            writer.writerow(self.headers) # escribe una fila nueva según el orden de fieldnames
+            
+    
