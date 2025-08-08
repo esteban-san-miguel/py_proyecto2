36bb7fce713d42977992ada68a0e945e25730d45
Esteban <estebansanmiguel78@gmail.com>
2025-08-07 22:32:50 -0300

    stage 4

--- gestor_csv.py
+++ gestor_csv.py
@@ -3,28 +3,28 @@ import os
 
 class Gestor_CSV: # clase
     def __init__(self, archivo): # constructor (propiedades y estado inicial del objeto)
-        self.archivo = archivo
+        self.archivo = archivo # Permite acceder a los atributos del objeto como el archivo
         self.headers = [
             "fecha", "nombre", "apellido", "celular", "email", 
             "marca", "item", "modelo", "n/s", "precio s/iva usd",
             "precio c/iva usd", "cambio $", "precio s/iva $", "precioc/iva $",
             "forma pago", "factura tipo", "factura nro", "comisión usd", "comisión $"
         ]
-        self.inicializar_archivo()
+        self.inicializar_archivo() # Llamada al método para inicializar el archivo
 
     def inicializar_archivo(self): # método de clase (acciones que realizará el objeto)
         if not os.path.exists(self.archivo): # verifica si existe el archivo sino lo crea
             with open(self.archivo, "w", newline="") as f: # abre el archivo en modo escritura
                 writer = csv.writer(f) # instancía la clase, crea el objeto writer
-                writer.writerow(self.headers) # escribe los encabezados en la fila 1 (0)
+                writer.writerow(self.headers) # llamada al método que escribe los encabezados en la fila 1 (0)
                 
     def guardar(self, fila): # método de clase c/2 args (fila es un diccionario contenedor del archivo csv)
         archivo_vacio = not os.path.exists(self.archivo) or os.path.getsize(self.archivo) == 0
         with open(self.archivo, "a", newline="") as f: # abre, agrega y cierra el archivo csv
             writer = csv.DictWriter(f, fieldnames=self.headers) # crea el objeto writer y escribe datos en el archivo desde diccionarios
             if archivo_vacio:
-                writer.writeheader() # escribe los encabezados si el archivo está vacío
-            writer.writerow(fila) # escribe la fila pasada como un diccionario
+                writer.writeheader() # llamada al método que escribe los encabezados si el archivo está vacío
+            writer.writerow(fila) # llamada al método que escribe la fila pasada como un diccionario
             
     def cargar(self): # método de clase
         if not os.path.exists(self.archivo): # verifica si existe el archivo sino lo crea
@@ -39,12 +39,10 @@ class Gestor_CSV: # clase
             datos.pop(index) # elimina el elemento de la posición index y corre una fila hacia arriba
             with open(self.archivo, "w", newline="") as f: # abre, escribe y cierra el archivo
                 writer = csv.DictWriter(f, fieldnames=self.headers) # crea el objeto reader y escribe filas como diccionarios según orden y columnas
-                writer.writeheader() # escribe 1er fila con los encabezados
-                writer.writerows(datos) # escribe filas como diccionarios, excepto la eliminada
+                writer.writeheader() # llamada al método que escribe 1er fila con los encabezados
+                writer.writerows(datos) # llamada al método que escribe filas como diccionarios, excepto la eliminada
                 
     def eliminar_todo(self): # método de clase
         with open(self.archivo, "w", newline="") as f: # abre, escribe y cierra el csv y borra todo su contenido
             writer = csv.writer(f) # crea el objeto writer csv, escribe filas csv en el archivo f
-            writer. writerow(self.headers) # self.headers es una lista con los encabezados y escribiéndola como única fila
-        
-                
+            writer. writerow(self.headers) # llamada al método en la que self.headers es una lista con los encabezados y escribiéndola como única fila
